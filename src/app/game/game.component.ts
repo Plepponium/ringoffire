410038075306045e81832a72d015ac7094950516
Plepponium <kleinchmida@gmail.com>
2025-04-18 20:31:27 +0200

    first commit

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
+
+
+@Component({
+  selector: 'app-game',
+  imports: [CommonModule, PlayerComponent],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+
+  }
+
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop()!;
+      this.pickCardAnimation = true;
+     
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000)
+    }
+  }
+}
