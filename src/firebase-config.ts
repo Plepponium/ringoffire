e3f0da2f665a395cd29817377048d21d6248c8b3
Plepponium <kleinchmida@gmail.com>
2025-05-02 16:54:11 +0200

    Remove firebase-config.ts from repo and ignore it

--- src/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBtp5C7os5znfhUdZZHzyIh0kRz43jhFG8",
-  authDomain: "ring-of-fire-b6f93.firebaseapp.com",
-  projectId: "ring-of-fire-b6f93",
-  storageBucket: "ring-of-fire-b6f93.firebasestorage.app",
-  messagingSenderId: "374699443927",
-  appId: "1:374699443927:web:8679c36f20fd8a35739292"
-  };
-  
